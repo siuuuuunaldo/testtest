11ed45b36c781e8d6c0d7448b78b0c8cc697d93c
kimyeonkyu7453 <hjk902902@naver.com>
2024-12-01 19:11:29 +0900

    Fix missing index.js file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 // src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css'; // 스타일 파일 (필요한 경우)
-import App from './App'; // App 컴포넌트 (필요한 경우)
 
 ReactDOM.render(
   <React.StrictMode>
